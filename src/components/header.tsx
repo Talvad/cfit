5c12014382c71347957654d3dfe71d0af0926242
Subash <subashsuni55@gmail.com>
2026-03-22 16:54:12 +0530

    updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "@tanstack/react-router";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import Logo from "./logo";
 
 // import ThemeToggle from "./ThemeToggle";
@@ -32,58 +34,53 @@ const navLinks = [
 export default function Header() {
 	return (
 		<header className="sticky top-0 z-50 bg-[#131313] border-b border-white/10 shadow-2xl py-4 backdrop-blur-lg">
-			<nav className="px-20 flex items-center gap-6">
+			<nav className="px-4 md:px-20 flex items-center gap-6">
 				<Logo />
-				<ul className="hidden md:flex gap-6">
-					{navLinks.map((link) => (
-						<li key={link.href}>
-							<Link
-								to={link.href}
-								className="font-headline uppercase tracking-tighter font-bold text-[#e5e2e1] hover:text-[#ffb3ac] transition-colors"
-								activeProps={{
-									className: "text-[#ffb3ac] border-b-2 border-[#d32f2f] pb-1",
-								}}
-								inactiveProps={{
-									className:
-										"text-[#e5e2e1] hover:text-[#ffb3ac] hover:border-b-2 hover:border-[#d32f2f] transition-colors",
-								}}
-							>
-								{link.name}
-							</Link>
-						</li>
-					))}
-				</ul>
-
+				<NavLinks className="order-3 md:order-2" />
 				<button
 					type="button"
-					className="ml-auto bg-(--primary-container) text-(--on-primary-container) px-6 py-2 rounded-full font-headline font-extrabold tracking-tighter hover:scale-105 active:scale-95 transition-all"
+					className="ml-auto order-2 md:order-3 bg-(--primary-container) text-(--on-primary-container) px-4 md:px-6 py-2 rounded-full font-headline md:font-extrabold font-bold tracking-tighter hover:scale-105 active:scale-95 transition-all"
 				>
 					JOIN NOW
 				</button>
 			</nav>
-			{/* <nav className="page-wrap flex flex-wrap items-center gap-x-3 gap-y-2 py-3 sm:py-4">
-				<Logo />
-
-				<div className="flex w-full flex-wrap items-center gap-x-4 gap-y-1 pb-1 text-sm font-semibold sm:order-2 sm:w-auto sm:flex-nowrap sm:pb-0">
-					<Link
-						to="/"
-						className="nav-link"
-						activeProps={{ className: "nav-link is-active" }}
-					>
-						Home
-					</Link>
-					<Link
-						to="/about"
-						className="nav-link"
-						activeProps={{ className: "nav-link is-active" }}
-					>
-						About
-					</Link>
-				</div>
-				<div className="ml-auto flex items-center gap-1.5 sm:ml-0 sm:gap-2">
-					<ThemeToggle />
-				</div>
-			</nav> */}
 		</header>
 	);
 }
+
+const NavLinks = ({ className, ...props }: React.ComponentProps<"div">) => {
+	const [openMenu, setOpenMenu] = useState(false);
+	const toggleOpenMenu = () => {
+		setOpenMenu(!openMenu);
+	};
+	return (
+		<div className={` ${className}`} {...props}>
+			<button type="button" className="md:hidden" onClick={toggleOpenMenu}>
+				{openMenu ? <X /> : <Menu />}
+			</button>
+
+			<ul
+				className={`flex flex-col md:flex-row gap-6 absolute md:relative top-full left-0 right-0 bg-[#131313] md:py-0 py-5 md:px-0 px-10 md:border-t-0 border-t border-white/10 md:shadow-none shadow-2xl ${!openMenu && "hidden md:flex"}`}
+			>
+				{navLinks.map((link) => (
+					<li key={link.href}>
+						<Link
+							to={link.href}
+							className="font-headline uppercase tracking-tighter font-bold text-[#e5e2e1] hover:text-[#ffb3ac] transition-colors"
+							activeProps={{
+								className: "text-[#ffb3ac] border-b-2 border-[#d32f2f] pb-1",
+							}}
+							inactiveProps={{
+								className:
+									"text-[#e5e2e1] hover:text-[#ffb3ac] hover:border-b-2 hover:border-[#d32f2f] transition-colors",
+							}}
+							onClick={() => setOpenMenu(false)}
+						>
+							{link.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
