1d1658a3365ed2f5ec133a7f6fba934bb8471ec0
Subash <subashsuni55@gmail.com>
2026-03-22 18:58:13 +0530

    feat: Externalize footer content into a dedicated resource object and update the Footer component to consume it using `@tanstack/react-router` Link components.

--- src/resources/index.tsx
+++ src/resources/index.tsx
@@ -11,18 +11,6 @@ type Header = {
 	cta: string;
 	logo: ReactNode;
 };
-type Website = {
-	header: Header;
-	footer: string;
-	pages: {
-		home: string;
-		mission: string;
-		gallery: string;
-		achievements: string;
-		pricing: string;
-		contact: string;
-	};
-};
 const menu: NavLinks = [
 	{
 		name: "Home",
@@ -54,9 +42,93 @@ export const header: Header = {
 	cta: "JOIN NOW",
 	logo: <Logo />,
 };
+type GroupNav = {
+	title: string;
+	menu: NavLinks;
+};
+type Item = {
+	key: string;
+	value: string;
+};
+type Footer = {
+	logo: ReactNode;
+	description: string;
+	links: GroupNav;
+	contact: {
+		title: string;
+		items: Item[];
+	};
+	rights: {
+		title: string;
+		subtitle: string;
+	};
+};
+const footerMenu: NavLinks = [
+	{
+		name: "Careers",
+		href: "/careers",
+	},
+	{
+		name: "Location",
+		href: "/location",
+	},
+	{
+		name: "Privacy Policy",
+		href: "/privacy-policy",
+	},
+	{
+		name: "Terms of Service",
+		href: "/terms-of-service",
+	},
+];
+const footerContact: Item[] = [
+	{
+		key: "headquarters",
+		value: "kannanagam,K.K Dist",
+	},
+	{
+		key: "email",
+		value: "[EMAIL_ADDRESS]",
+	},
+	{
+		key: "phone",
+		value: "1234567890",
+	},
+];
+export const footer: Footer = {
+	logo: <Logo />,
+	description:
+		"The premier destination for high-intensity athletic performance. We redefine the physical limit through industrial excellence.",
+	links: {
+		title: "Quick Links",
+		menu: footerMenu,
+	},
+	contact: {
+		title: "Contact",
+		items: footerContact,
+	},
+	rights: {
+		title: "© 2024 C-FIT. ALL RIGHTS RESERVED.",
+		subtitle: "FORCE. FLOW. FINISH.",
+	},
+};
+
+type Website = {
+	header: Header;
+	footer: Footer;
+	pages: {
+		home: string;
+		mission: string;
+		gallery: string;
+		achievements: string;
+		pricing: string;
+		contact: string;
+	};
+};
+
 export const website: Website = {
 	header,
-	footer: "",
+	footer,
 	pages: {
 		home: "",
 		mission: "",
